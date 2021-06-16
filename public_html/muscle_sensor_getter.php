0e7189ef5190446c1d2dfcf1781ebae13ed09ff8
Daiki Suzuki <suzuki11235813@gmail.com>
2021-06-16 21:50:23 +0900

    fix:if $stream_out is false

--- public_html/muscle_sensor_getter.php
+++ public_html/muscle_sensor_getter.php
@@ -13,6 +13,9 @@ ssh2_auth_password($sconnection,USER,PASSWORD);
 $stream = ssh2_exec($sconnection,"/usr/bin/sudo python3 /home/pi/public_html/muscle_sensor.py");
 stream_set_blocking($stream, true);
 $stream_out = ssh2_fetch_stream($stream, SSH2_STREAM_STDIO);
+if($stream_out == false){
+  $stream_out = 0;
+}
 echo stream_get_contents($stream_out);
 ?>
 </body>
